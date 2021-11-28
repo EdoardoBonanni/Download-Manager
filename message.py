ea5f76d3940b478dc40e77ba952e205e3a3f3a4a
Edoardo Bonanni <edobona98@gmail.com>
2021-11-28 16:07:00 +0100

    Delete message.py

--- message.py
+++
@@ -1,9 +0,0 @@
-class Message:
-    def __init__(self, message):
-        self.message = message
-
-    def receiveData(self):
-        return self.message
-
-    def changeData(self, data):
-        self.message = data
